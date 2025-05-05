bc9218ac3a335b5285219f89a996d9a31fcbc8ef
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-05-05 09:07:22 +0100

    style: Style fixes based on the .clang format.

--- include/typing/ctypes.hpp
+++ include/typing/ctypes.hpp
@@ -2,10 +2,10 @@
  * @file include/typing/ctypes.hpp
  * @brief Defines common C-style types for the itl namespace.
  * @category Typing
- * 
+ *
  * This header provides typedefs for commonly used C-style types
  * within the itl namespace.
- * 
+ *
  * @author Ismael Moreira <ismaelmoreirakt@gmail.com>
  * @date 05.05.2025
  */
@@ -14,21 +14,21 @@
 
 /**
  * @namespace itl
- * @brief A general-purpose namespace for the itl library, containing 
+ * @brief A general-purpose namespace for the itl library, containing
  * custom STL components, allocators, types, and utilities.
  */
 namespace itl {
-  /**
-   * @typedef size_t
-   * @brief Unsigned integer type for representing sizes.
-   */
-  typedef unsigned long size_t;
+/**
+ * @typedef size_t
+ * @brief Unsigned integer type for representing sizes.
+ */
+typedef unsigned long size_t;
 
-  /**
-   * @typedef ssize_t
-   * @brief Signed integer type for representing sizes or counts.
-   */
-  typedef signed long ssize_t;
+/**
+ * @typedef ssize_t
+ * @brief Signed integer type for representing sizes or counts.
+ */
+typedef signed long ssize_t;
 } // namespace itl
 
 #endif // _ITL_TYPING_C_TYPES_HPP
