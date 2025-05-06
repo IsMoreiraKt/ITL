69f08c4a51133ed0bdd59edc2092c01b0d9e1464
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-05-06 20:51:13 +0100

    feat: Added the startPosition property to the allocation metadata.

--- include/memory/allocator.hpp
+++ include/memory/allocator.hpp
@@ -34,6 +34,7 @@ namespace itl {
  */
 typedef struct {
     bool used; ///< Indicates whether the allocation is currently in use.
+    itl::size_t startPosition; ///< Start position of the memory page.
     itl::size_t size; ///< The size of the allocated memory, in bytes.
     void* ptr; ///< A pointer to the allocated memory.
 } AllocationMetadata;
@@ -68,20 +69,10 @@ typedef struct {
  * @brief Allocates a block of memory of the specified size.
  *
  * @param size The size of the memory block to allocate, in bytes.
- * @return A pointer to the allocated memory block, or `nullptr` if
+ * @return A pointer to the allocated memory block, or nullptr if
  * the allocation fails.
  */
-static void* alloc(itl::size_t size);
-
-/**
- * @brief Allocates a block of memory with the specified alignment.
- *
- * @param alignment The alignment requirement for the memory block.
- * @param size The size of the memory block to allocate, in bytes.
- * @return A pointer to the aligned memory block, or `nullptr` if the
- * allocation fails.
- */
-static void* alignedAlloc(itl::size_t alignment, itl::size_t size);
+void* alloc(itl::size_t size);
 
 /**
  * @brief Allocates memory for an array and initializes it to zero.
@@ -89,27 +80,27 @@ static void* alignedAlloc(itl::size_t alignment, itl::size_t size);
  * @param number The number of elements in the array.
  * @param size The size of each element, in bytes.
  * @return A pointer to the allocated and zero-initialized memory block,
- * or `nullptr` if the allocation fails.
+ * or nullptr if the allocation fails.
  */
-static void* calloc(itl::size_t number, itl::size_t size);
+void* calloc(itl::size_t number, itl::size_t size);
 
 /**
  * @brief Reallocates a previously allocated memory block to a new size.
  *
  * @param pointerToMemory A pointer to the memory block to reallocate.
  * @param newSize The new size of the memory block, in bytes.
- * @return A pointer to the reallocated memory block, or `nullptr`
+ * @return A pointer to the reallocated memory block, or nullptr
  * if the reallocation fails.
  */
-static void* realloc(void* pointerToMemory, itl::size_t newSize);
+void* realloc(void* pointerToMemory, itl::size_t newSize);
 
 /**
  * @brief Frees a previously allocated memory block.
  *
  * @param pointerToMemory A pointer to the memory block to free.
- * If the pointer is `nullptr`, no operation is performed.
+ * If the pointer is nullptr, no operation is performed.
  */
-static void free(void* pointerToMemory);
+void free(void* pointerToMemory);
 } // namespace itl
 
 #endif // _ITL_MEMORY_ALLOCATOR_HPP
