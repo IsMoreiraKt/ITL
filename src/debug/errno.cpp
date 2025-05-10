af5c1db4a9869897411a9b3e354cddbc89c226fa
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-05-10 11:09:57 +0100

    feat: Added implementation of errno_str function.

--- src/debug/errno.cpp
+++ src/debug/errno.cpp
@@ -12,8 +12,31 @@
  * @date May 10th, 2025
  */
 #include "debug/errno.hpp"
+#include "typing/ctypes.hpp"
 
+/**
+ * @namespace itl
+ * @brief A general-purpose namespace for the ITL library, containing
+ * custom STL components, allocators, types, and utilities.
+ */
+namespace itl {
 /**
  * @brief Thread-local variable for storing error codes.
  */
 __thread int errno = 0;
+
+/**
+ * @brief Returns a human-readable description of an error code.
+ *
+ * This function maps an error code (stored in errno) to a string
+ * describing the error. It is useful for debugging and logging purposes.
+ *
+ * @return A constant string containing the description of the error.
+ */
+itl::cstring errno_str(void)
+{
+    if (itl::errno <= 0 || itl::errno > 34)
+        return nullptr;
+    return itl::_error_description[itl::errno - 1];
+}
+} /// namespace itl
