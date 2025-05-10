6413258e7425e4a7a098e7aa9b7faa1923771768
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-05-10 08:11:56 +0100

    feat: Addition of the implementation file which does the actual implementation of the errno related functions.

---
+++ src/debug/errno.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file src/debug/errno.cpp
+ * @brief Implements the thread-local `errno` variable for error handling.
+ * @category Error Handling
+ *
+ * This file provides the definition of the thread-local errno variable,
+ * which is used to store error codes set by system calls and
+ * library functions. Each thread has its own instance of errno, ensuring
+ * thread-safe error handling.
+ *
+ * @author Ismael Moreira <ismaelmoreirakt@gmail.com>
+ * @date May 10th, 2025
+ */
+#include "debug/errno.hpp"
+
+/**
+ * @brief Thread-local variable for storing error codes.
+ */
+__thread int errno = 0;
