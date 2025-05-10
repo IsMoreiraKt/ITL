9b1f34fc47a84b349c2e840b7126945a5f3146a2
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-05-10 09:41:30 +0100

    feat: Added macros that extract arguments to make system calls independent of the system architecture.

--- include/__internal/system/syscalls/sys_arguments.hpp
+++ include/__internal/system/syscalls/sys_arguments.hpp
@@ -33,6 +33,42 @@ namespace __internal {
      */
     namespace system {
 #if defined __x86_64__
+/**
+ * @def first_register
+ * @brief Defines the register used for the first argument in system calls.
+ */
+#define first_register rdi
+
+/**
+ * @def second_register
+ * @brief Defines the register used for the second argument in system calls.
+ */
+#define second_register rsi
+
+/**
+ * @def third_register
+ * @brief Defines the register used for the third argument in system calls.
+ */
+#define third_register rdx
+
+/**
+ * @def fourth_register
+ * @brief Defines the register used for the fourth argument in system calls.
+ */
+#define fourth_register r10
+
+/**
+ * @def fifth_type
+ * @brief Defines the register used for the fifth argument in system calls.
+ */
+#define fifth_type r8
+
+/**
+ * @def sixth_register
+ * @brief Defines the register used for the sixth argument in system calls.
+ */
+#define sixth_register r9
+
         /**
          * @struct __SysArguments
          * @brief Encapsulates arguments passed to system calls on x86_64
@@ -51,6 +87,42 @@ namespace __internal {
         } __SysArguments;
 
 #else /// i386 architecture
+/**
+ * @def first_register
+ * @brief Defines the register used for the first argument in system calls.
+ */
+#define first_register ebx
+
+/**
+ * @def second_register
+ * @brief Defines the register used for the second argument in system calls.
+ */
+#define second_register ecx
+
+/**
+ * @def third_register
+ * @brief Defines the register used for the third argument in system calls.
+ */
+#define third_register edx
+
+/**
+ * @def fourth_register
+ * @brief Defines the register used for the fourth argument in system calls.
+ */
+#define fourth_register esi
+
+/**
+ * @def fifth_type
+ * @brief Defines the register used for the fifth argument in system calls.
+ */
+#define fifth_type ebp
+
+/**
+ * @def sixth_register
+ * @brief Defines the register used for the sixth argument in system calls.
+ */
+#define sixth_register edi
+
         /**
          * @struct __SysArguments
          * @brief Encapsulates arguments passed to system calls on i386
@@ -68,6 +140,89 @@ namespace __internal {
             void* ebp; ///< Sixth argument  (base pointer register).
         } __SysArguments;
 #endif /// __x86_64__
+
+/**
+ * @def SYS_ARG1
+ * @brief Extracts the first argument from the system call arguments
+ * structure.
+ */
+#define SYS_ARG1(arguments, first_type, first_name) \
+    first_type first_name = (first_type)((arguments)->first_register);
+
+/**
+ * @def SYS_ARG2
+ * @brief Extracts the first and second arguments from the system call
+ * arguments structure.
+ */
+#define SYS_ARG2(arguments,                                            \
+    first_type, first_name,                                            \
+    second_type, second_name)                                          \
+    first_type first_name = (first_type)((arguments)->first_register); \
+    second_type second_name = (second_type)((arguments)->second_register);
+
+/**
+ * @def SYS_ARG3
+ * @brief Extracts the first, second, and third arguments from the system
+ * call arguments structure.
+ */
+#define SYS_ARG3(arguments,                                                \
+    first_type, first_name,                                                \
+    second_type, second_name,                                              \
+    third_type, third_name)                                                \
+    first_type first_name = (first_type)((arguments)->first_register);     \
+    second_type second_name = (second_type)((arguments)->second_register); \
+    third_type third_name = (third_type)((arguments)->third_register);
+
+/**
+ * @def SYS_ARG4
+ * @brief Extracts the first, second, third, and fourth arguments from the
+ * system call arguments structure.
+ */
+#define SYS_ARG4(arguments,                                                \
+    first_type, first_name,                                                \
+    second_type, second_name,                                              \
+    third_type, third_name,                                                \
+    fourth_type, fourth_name)                                              \
+    first_type first_name = (first_type)((arguments)->first_register);     \
+    second_type second_name = (second_type)((arguments)->second_register); \
+    third_type third_name = (third_type)((arguments)->third_register);     \
+    fourth_type fourth_name = (fourth_type)((arguments)->fourth_register);
+
+/**
+ * @def SYS_ARG5
+ * @brief Extracts the first, second, third, fourth, and fifth arguments
+ * from the system call arguments structure.
+ */
+#define SYS_ARG5(arguments,                                                \
+    first_type, first_name,                                                \
+    second_type, second_name,                                              \
+    third_type, third_name,                                                \
+    fourth_type, fourth_name,                                              \
+    fifth_type, fifth_name)                                                \
+    first_type first_name = (first_type)((arguments)->first_register);     \
+    second_type second_name = (second_type)((arguments)->second_register); \
+    third_type third_name = (third_type)((arguments)->third_register);     \
+    fourth_type fourth_name = (fourth_type)((arguments)->fourth_register); \
+    fifth_type fifth_name = (fifth_type)((arguments)->fifth_register);
+
+/**
+ * @def SYS_ARG6
+ * @brief Extracts the first, second, third, fourth, fifth, and sixth
+ * arguments from the system call arguments structure.
+ */
+#define SYS_ARG6(arguments,                                                \
+    first_type, first_name,                                                \
+    second_type, second_name,                                              \
+    third_type, third_name,                                                \
+    fourth_type, fourth_name,                                              \
+    fifth_type, fifth_name,                                                \
+    sixth_type, sixth_name)                                                \
+    first_type first_name = (first_type)((arguments)->first_register);     \
+    second_type second_name = (second_type)((arguments)->second_register); \
+    third_type third_name = (third_type)((arguments)->third_register);     \
+    fourth_type fourth_name = (fourth_type)((arguments)->fourth_register); \
+    fifth_type fifth_name = (fifth_type)((arguments)->fifth_register);     \
+    sixth_type sixth_name = (sixth_type)((arguments)->sixth_register);
     }; /// system
 } /// __internal
 } /// itl
